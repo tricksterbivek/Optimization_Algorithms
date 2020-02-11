a3d888b681d58395c02aefe3937b819627bf6b2f
Trickster <tricksterbivek@gmail.com>
2020-02-11 12:03:49 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Optimization_Algorithm
+ THe nn function calculate the change in weights from error function and gradient descent is used to optimize the result
---
+++ nn.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+learnrate = 0.05
+x = np.array([1, 2])
+y = np.array(0.5)
+
+w = np.array([0.5, -0.5])
+
+nn_output = sigmoid(np.dot(x, w))
+
+error = y - nn_output
+
+del_w = learnrate * error * nn_output * (1 - nn_output) * x
+
+print('Neural Network output:')
+print(nn_output)
+print('Amount of Error:')
+print(error)
+print('Change in Weights:')
+print(del_w)
